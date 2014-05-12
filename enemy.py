8a9e56a03b44cf84222d6ca7459612bd8a6069a5
Tomer Gilad <tomergil@gmail.com>
2014-05-12 20:56:25 +0300

    initial commit

---
+++ enemy.py
@@ -0,0 +1,62 @@
+import pygame
+import tmx
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super(Enemy, self).__init__(*groups)
+
+        self.speed_x = 0
+        self.speed_y = 0
+        self.direction_x = 0
+        self.direction_y = 0
+
+    def move(self, dt):
+        self.rect.x += self.speed_x * dt * self.direction_x
+        self.rect.y += self.speed_y * dt * self.direction_y
+    
+    def update(self, dt, game, *args):
+        last = self.rect.copy()
+
+        self.move(dt)
+        for tile in game.level.layers['triggers'].collide(self.rect, 'reverse'):
+            if tile.color == game.disabled_color:
+                continue
+            self.rect = last
+            if 'x' in tile['reverse']:
+                self.direction_x *= -1
+            if 'y' in tile['reverse']:
+                self.direction_y *= -1
+            break
+
+        for tile in game.level.layers['triggers'].collide(self.rect, 'move'):
+            if tile.color == game.disabled_color:
+                continue
+            self.rect = last
+            if 'left' == tile['move']:
+                self.direction_x = -1
+                self.direction_y = 0
+            if 'right' == tile['move']:
+                self.direction_x = 1
+                self.direction_y = 0
+            if 'up' == tile['move']:
+                self.direction_x = 0
+                self.direction_y = -1
+            if 'down' == tile['move']:
+                self.direction_x = 0
+                self.direction_y = 1
+            break
+
+class SimpleEnemy(Enemy):
+    def __init__(self, x, y, *groups):
+        super(SimpleEnemy, self).__init__(*groups)
+
+        self.image = pygame.Surface((28, 60))
+        self.image.fill((200, 100, 250))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed_x = 50
+        self.direction_x = 1
+
+def create_enemy(name, x, y, color, *groups):
+    return SimpleEnemy(x, y, *groups)
---
+++ main.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python
+import pygame
+import tmx
+import enemy
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, start_x, start_y, *groups):
+        super(Player, self).__init__(*groups)
+        
+        self.image = pygame.Surface((28, 60))
+        self.image.fill((200, 200, 200))
+        self.rect = self.image.get_rect()
+        self.rect.x = start_x
+        self.rect.y = start_y
+
+        self.resting = False
+        self.speed_y = 0
+        self.speed_x = 0
+        self.direction = 0
+
+    def update(self, dt, game, *args):
+        last = self.rect.copy()
+
+        self.rect.x += self.speed_x * dt * self.direction
+        self.rect.y += self.speed_y * dt
+
+        self.direction = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.speed_x = 100.0 + self.rect.width
+            self.direction = 1
+        if keys[pygame.K_LEFT]:
+            self.speed_x = 100.0
+            self.direction = -1
+        if keys[pygame.K_LEFT] and keys[pygame.K_RIGHT]:
+            self.speed_x = 0
+            self.direction = 0
+
+
+        for tile in game.level.layers['triggers'].collide(self.rect, 'blocker'):
+            colliding_side = False
+            color = tile['color']
+            if color == game.disabled_color:
+                continue
+
+            if self.rect.right == tile.left or self.rect.left == tile.right:
+                colliding_side = True
+
+            if last.right <= tile.left and self.rect.right > tile.left:
+                print 'colliding right'
+                self.rect.right = tile.left
+            if last.left >= tile.right and self.rect.left < tile.right:
+                print 'colliding left'
+                self.rect.left = tile.right
+            if last.bottom <= tile.top and self.rect.bottom > tile.top and not colliding_side:
+                print 'colliding bottom'
+                self.resting = True
+                self.rect.bottom = tile.top
+                self.speed_y = 0
+            if last.top >= tile.bottom and self.rect.top < tile.bottom and not colliding_side:
+                print 'colliding top'
+                self.rect.top = tile.bottom
+                self.speed_y = 50
+
+
+        if self.resting and keys[pygame.K_SPACE]:
+            self.resting = False
+            self.speed_y = -500
+        else:
+            self.speed_y = min((500, self.speed_y + 20))
+
+        for tile in game.level.layers['triggers'].collide(self.rect, 'death'):
+            color = tile['color']
+            if color == game.disabled_color:
+                continue
+            print 'im dead'
+            break
+
+        for enemy in pygame.sprite.spritecollide(self, game.enemies, False):
+            if enemy.color == game.disabled_color:
+                continue
+            print 'im dead'
+            break
+
+
+class Game(object):
+
+    def __init__(self, level_path, screen_size, screen, fps=60):
+        self.level = tmx.load(level_path, screen_size)
+        spawn = self.level.layers['triggers'].find('spawn')[0]
+
+        self.sprite_layer = tmx.SpriteLayer()
+        self.player = Player(spawn.px, spawn.py, self.sprite_layer)
+        self.level.layers.append(self.sprite_layer)
+
+        self.clock = pygame.time.Clock()
+        self.fps = fps
+        self.disabled_color = ""
+
+        self.enemies = tmx.SpriteLayer()
+        try:
+            for enemy in self.level.layers['enemies']:
+                enemy.create_enemy(enemy['name'], enemy.px, enemy.py, enemy['color'], self.enemies)
+        except Exception as e:
+            print e
+
+        self.level.layers.append(self.enemies)
+
+
+        self.screen = screen
+
+    def loop(self):
+        while True:
+            dt = self.clock.tick(self.fps) / 1000.0
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    return
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_1:
+                        self.toggle_layer("red")
+                    elif event.key == pygame.K_2:
+                        self.toggle_layer("green")
+                    elif event.key == pygame.K_3:
+                        self.toggle_layer("blue")
+                    elif event.key == pygame.K_4:
+                        self.toggle_layer("orange")
+
+            self.screen.fill((0,0,0))
+            self.level.update(dt, self)
+            self.level.set_focus(self.player.rect.x, self.player.rect.y)
+            self.level.draw(self.screen)
+
+            pygame.display.flip()
+
+    def toggle_layer(self, color):
+        print "toggling %s" % color
+        visible = not self.level.layers[color].visible
+        if self.disabled_color != "":
+            self.level.layers[self.disabled_color].visible = True
+        self.level.layers[color].visible = visible
+        if visible:
+            self.disabled_color = ""
+        else:
+            self.disabled_color = color
+
+def main():
+    pygame.init()
+    screen_size = (640, 480)
+    screen = pygame.display.set_mode(screen_size)
+
+    game = Game('level-0.tmx', screen_size, screen)
+    game.loop()
+
+if __name__ == '__main__':
+    main()
